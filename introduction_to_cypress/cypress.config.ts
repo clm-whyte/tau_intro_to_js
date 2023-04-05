fb59699206417a83fca278d4ee21813779f0ebec
Calum Maciver-Whyte <clm.whyte@gmail.com>
2023-04-05 11:33:31 +0100

    wrote tests in Typescript

---
+++ introduction_to_cypress/cypress.config.ts
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on: any, config: any) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/integration/**/*.spec.{js,jsx,ts,tsx}'
+  },
+});
